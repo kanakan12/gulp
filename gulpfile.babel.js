0c0c1f069c99aa38de7ccceaca2e2cf2f12d12cf
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-19 20:53:50 +0900

    Test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,16 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+export const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+export const clean = () => del(["build"]);
+
+export const dev = gulp.series([clean, pug]);
